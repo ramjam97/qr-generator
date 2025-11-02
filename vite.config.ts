0454813c3a7bc61a698521dc299b7e8fb8f30e93
Ramjam97 <ram.jamolod@gmail.com>
2025-11-02 18:39:50 +0800

    fixing deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'path'
+import dotenv from 'dotenv'
 
 // Get __dirname equivalent in ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+dotenv.config()
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/qr-generator/',
+  base: process.env.VITE_BASE || '/',
   plugins: [
     react(),
     tailwindcss()
